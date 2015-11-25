aa5adafe715ee48450872fba33be211bd7d2cff7
yil332 <yil332@eng.ucsd.edu>
2015-11-24 20:19:05 -0800

    add gitignore

---
+++ TankBattle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TankBattle
+//
+//  Created by Yiqiu Liu on 11/24/15.
+//  Copyright © 2015 SUTeam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TankBattle/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TankBattle
+//
+//  Created by Yiqiu Liu on 11/24/15.
+//  Copyright © 2015 SUTeam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
